0ad19f0c786f6a5c9d21e3efa0b43251431d7ae8
Zach Bridwell <bridwellzac@ymail.com>
2022-04-20 20:15:10 -0500

    added card ui

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,22 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const scores = [
+    {
+      id: 'e1',
+      score: 82,
+      couse: 'West Chase',
+      date: '10/12/2021',
+    },
+    {
+      id: 'e2',
+      score: 91,
+      couse: 'Agusta National',
+      date: '2/30/2022',
+    },
+    {
+      id: 'e3',
+      score: 78,
+      couse: 'Twin Bridges GC',
+      date: '11/21/2021',
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>tbd</p>
     </div>
   );
 }
